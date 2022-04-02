3af1cb2deb4f968f42ab3427610b69cd0e2bc344
JFrdrkssn <johanf9401@gmail.com>
2022-04-02 20:07:25 +0000

    Add Post model for posts on the blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Post(models.Model):
+    """
+    Model for posts on the blog.
+    """
+    title = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=150, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    featured_image = CloudinaryField('image', default='placeholder')
+    excerpt = models.TextField(blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    likes = models.ManyToManyField(User, related_name='blog_likes', blank=True)
+
+    class Meta:
+        """
+        Orders blog posts based on time of creation.
+        Descending order, newest first.
+        """
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    # Helper method to return the total number of likes on a post
+    def number_of_likes(self):
+        return self.likes.count()
