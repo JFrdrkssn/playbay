66da5a2e875c9f11ff107e16923625625388d82b
JFrdrkssn <johanf9401@gmail.com>
2022-04-02 20:25:58 +0000

    Add Comment model for comments on posts

--- blog/models.py
+++ blog/models.py
@@ -32,3 +32,21 @@ class Post(models.Model):
     # Helper method to return the total number of likes on a post
     def number_of_likes(self):
         return self.likes.count()
+
+
+class Comment(models.Model):
+    """
+    Model for comments on the blog.
+    """
+    post = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.name} said {self.body}"
