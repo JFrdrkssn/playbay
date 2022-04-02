78b7162f0182c88af7336fd45bc223d25b3ce171
JFrdrkssn <johanf9401@gmail.com>
2022-04-02 21:06:27 +0000

    Add Summernote to content field on admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import Post
+from django_summernote.admin import SummernoteModelAdmin
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+    """
+    The content field in blog posts will use Summernote
+    """
+    summernote_fields = ('content')
