18c20c02c491c1061b07a98912928d393a717ab2
JFrdrkssn <johanf9401@gmail.com>
2022-04-04 14:51:38 +0000

    Add list_filter and search_fields to Comment section of admin page

--- blog/admin.py
+++ blog/admin.py
@@ -23,3 +23,5 @@ class CommentAdmin(SummernoteModelAdmin):
     """
 
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ['name', 'email', 'body']
