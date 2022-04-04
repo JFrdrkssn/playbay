f46dfe1b966ad7bc0de1359938068cf2153b08fd
JFrdrkssn <johanf9401@gmail.com>
2022-04-04 14:44:26 +0000

    Add list display and search fields to blog posts on admin page

--- blog/admin.py
+++ blog/admin.py
@@ -9,6 +9,8 @@ class PostAdmin(SummernoteModelAdmin):
     The content field in blog posts will use Summernote
     """
 
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
