314e7b8592bc0e1eda115ef8ecbee1b32d28dfe6
JFrdrkssn <johanf9401@gmail.com>
2022-04-05 15:56:10 +0000

    Add POST method for comments, saving the comment to the correct blog post

--- blog/views.py
+++ blog/views.py
@@ -27,13 +27,48 @@ class PostDetail(View):
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
-        
+
         return render(
             request,
             'post_detail.html',
             {
                 'post': post,
                 'comments': comments,
+                'commented': False,
+                'liked': liked,
+                'comment_form': CommentForm()
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comments = post.comments.order_by('created_on')
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+
+        # Automatically passes in email and username
+        # to comment form when logged in
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.username = request.user.username
+            # Save the comment to the correct post
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+        else: 
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            'post_detail.html',
+            {
+                'post': post,
+                'comments': comments,
+                'commented': True,
                 'liked': liked,
                 'comment_form': CommentForm()
             },
