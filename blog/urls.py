07d94b1282c5acdd0f516206b46f41237b824266
JFrdrkssn <johanf9401@gmail.com>
2022-04-08 14:23:14 +0000

    Add URL path for adding posts view

--- blog/urls.py
+++ blog/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('post/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
+    path('add_post/', views.AddPost.as_view(), name='add_post'),
     path('like/<int:id>', views.PostLike.as_view(), name='post_like'),
 ]
