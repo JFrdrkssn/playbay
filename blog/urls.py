251bcc7540398122330ceab62d1711a434d937c1
JFrdrkssn <johanf9401@gmail.com>
2022-04-09 17:29:44 +0000

    Add edit_post URL and update post id to pk for generic views functionality

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
-    path('post/<int:id>/', views.PostDetail.as_view(), name='post_detail'),
+    path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('add_post/', views.AddPost.as_view(), name='add_post'),
-    path('like/<int:id>', views.PostLike.as_view(), name='post_like'),
+    path('post/<int:pk>/edit_post/', views.EditPost.as_view(), name='edit_post'),
+    path('like/<int:pk>', views.PostLike.as_view(), name='post_like'),
 ]
