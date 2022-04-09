0c0245e12ba619fe9817c8a49e1746da9cb5218e
JFrdrkssn <johanf9401@gmail.com>
2022-04-09 16:35:17 +0000

    Add blank=True to img field in Post model to remove asterisk from add post form

--- blog/models.py
+++ blog/models.py
@@ -13,7 +13,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    featured_image = CloudinaryField('image', default='placeholder')
+    featured_image = CloudinaryField('image', default='placeholder', blank=True)
     excerpt = models.TextField(max_length=200)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
