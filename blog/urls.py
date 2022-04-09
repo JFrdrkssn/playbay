30019e35cb36a78a3ec65e0a1a9f4be47e8e619f
JFrdrkssn <johanf9401@gmail.com>
2022-04-09 20:59:57 +0000

    Add URL path for DeletePost view

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetail.as_view(), name='post_detail'),
     path('add_post/', views.AddPost.as_view(), name='add_post'),
     path('post/<int:pk>/edit_post/', views.EditPost.as_view(), name='edit_post'),
+    path('post/<int:pk>/delete_post/', views.DeletePost.as_view(), name='delete_post'),
     path('like/<int:pk>', views.PostLike.as_view(), name='post_like'),
 ]
