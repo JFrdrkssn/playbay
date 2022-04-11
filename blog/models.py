5a217c8f402538ad9a32b5647d8888e4b1f658b2
JFrdrkssn <johanf9401@gmail.com>
2022-04-11 13:52:41 +0000

    Remove updated_on DateTimeField in Post model, not used

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,6 @@ class Post(models.Model):
     """
     title = models.CharField(max_length=150, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
-    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder', blank=True)
     excerpt = models.CharField(max_length=200)
