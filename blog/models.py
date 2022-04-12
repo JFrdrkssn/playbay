77db3197ce7b6ac949ff2d47b3a48f80d6c776af
JFrdrkssn <johanf9401@gmail.com>
2022-04-12 13:28:37 +0000

    Change self.name to self.user in Comment __str__ function for compability

--- blog/models.py
+++ blog/models.py
@@ -67,4 +67,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return f"{self.name} said {self.body}"
+        return f"{self.user} said {self.body}"
