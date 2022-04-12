58e77afdc879831d53d9415d84f5fc6e2b09884b
JFrdrkssn <johanf9401@gmail.com>
2022-04-12 21:58:21 +0000

    Add handlers for 500 and 404 error urls

--- playbay/urls.py
+++ playbay/urls.py
@@ -22,3 +22,6 @@ urlpatterns = [
     path('', include('blog.urls'), name='blog_urls'),
     path('accounts/', include('allauth.urls')),
 ]
+
+handler404 = 'blog.views.error_404'
+handler500 = 'blog.views.error_500'
